5c63afc55b84dfa73dae76a2bd5a5d25e9aa46d3
Gorp5 <69279346+Gorp5@users.noreply.github.com>
2025-03-04 13:10:23 -0500

    Added UserTest

---
+++ genrequest-domain/src/test/java/edu/gmu/cs321/UserTest.java
@@ -0,0 +1,13 @@
+package edu.gmu.cs321;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class UserTest {
+    
+    @Test
+    public void testUser() {
+        assertTrue(true);
+    }
+}
